91b8d1352d4757b030b792d26458b100dedf61fb
soju6jan <cybersol@naver.com>
2020-04-04 03:32:40 +0900

    1

--- logic.py
+++ logic.py
@@ -44,7 +44,11 @@ class Logic(object):
     @staticmethod
     def plugin_load():
         try:
-            logger.debug('%s plugin_load', package_name)
+            logger.debug('%s plugin_load', package_name)            
+            if platform.system() != 'Windows':
+                custom = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'bin')    
+                os.system("chmod 777 -R %s" % custom)
+
             Logic.db_init()
             if ModelSetting.get_bool('auto_start'):
                 Logic.scheduler_start()
